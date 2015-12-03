0d3b5a9a757973f76b5f552bcca0e77eed4b71e3
k13052kk <k13052kk@pck13052kk.local>
2015-12-03 16:53:37 +0900

    Initial Commit

---
+++ OCR3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OCR3
+//
+//  Created by k13052kk on 2015/12/03.
+//  Copyright (c) 2015年 k13052kk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
